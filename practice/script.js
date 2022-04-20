a8029b14eb4b8e478a27af492639b4fc0353859a
leedo97y <dlehdus1433@gmail.com>
2022-04-20 15:32:49 +0900

    progress-bar

--- practice/script.js
+++ practice/script.js
@@ -1,7 +1,33 @@
 ;(function () {
   'use strict'
 
+
+  let timerId
   const get = (target) => {
     return document.querySelector(target)
   }
+
+  // throttle : 성능 향상에 도움
+  const throttle = (callback, time) => {
+    if (timerId) return
+    timerId = setTimeout(() => {
+      callback()
+      timerId = undefined
+    }, time)
+  }
+
+  const $progressBar = get('.progress-bar')
+
+  const onScroll = () => {
+    const height = 
+    document.documentElement.scrollHeight - 
+    document.documentElement.clientHeight
+
+    const scrollTop = document.documentElement.scrollTop
+
+    const width = (scrollTop / height) * 100
+    $progressBar.style.width = width + '%'
+  }
+
+  window.addEventListener('scroll', () => throttle(onScroll, 100))
 })()
